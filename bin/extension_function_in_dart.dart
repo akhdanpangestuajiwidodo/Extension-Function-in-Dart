14adc6948328c6be9722d1e62b2282e15969f9b8
akhdanpangestuajiwidodo <akhdan1303@gmail.com>
2021-07-13 21:22:20 +0700

    Initial Commit

---
+++ bin/extension_function_in_dart.dart
@@ -0,0 +1,5 @@
+import 'package:extension_function_in_dart/extension_function_in_dart.dart' as extension_function_in_dart;
+
+void main(List<String> arguments) {
+  print('Hello world: ${extension_function_in_dart.calculate()}!');
+}
---
+++ test/extension_function_in_dart_test.dart
@@ -0,0 +1,8 @@
+import 'package:extension_function_in_dart/extension_function_in_dart.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
